289876fa4eeb8c3d33ae77c0e154dc16a940c17d
sslavov93 <sslavov93@gmail.com>
2016-08-25 09:10:07 +0300

    Added all package categories and full package list.

--- packages.py
+++ packages.py
@@ -1,31 +1,187 @@
-all_packages = {"": ""}
+all_packages = {"Aircrack-ng": "", "Amap": "", "Arduino": "", "Armitage": "",
+                "Asleap": "", "Automater": "", "BBQSQL": "", "BED": "",
+                "Backdoor Factory": "", "BeEF": "", "Binwalk": "",
+                "BlueMaho": "", "BlueRanger": "", "Bluelog": "",
+                "Bluepot": "", "Bluesnarfer": "", "Bully": "",
+                "Burp Suite": "", "CDPSnarf": "", "Capstone": "",
+                "CaseFile": "", "CeWL": "", "CmosPwd": "", "Commix": "",
+                "Cookie Cadger": "", "CryptCat": "", "Cuckoo": "",
+                "CutyCapt": "", "Cymothoa": "", "DBPwAudit": "", "DFF": "",
+                "DHCPig": "", "DMitry": "", "DNSChef": "", "DNSRecon": "",
+                "Doona": "", "DotDotPwn": "", "Dradis": "", "Dumpzilla": "",
+                "Fern Wifi Cracker": "", "Fierce": "", "Firewalk": "",
+                "Foremost": "", "FunkLoad": "", "GISKismet": "", "GSD": "",
+                "Galleta": "", "Ghost Phisher": "", "GoLismero": "",
+                "Gqrx": "", "Greenbone Security Assistant": "", "Guymager": "",
+                "HTTPTunnel": "", "HexInject": "", "HexorBase": "",
+                "InTrace": "", "Inguma": "", "Intersect": "", "Inundator": "",
+                "JD-GUI": "", "John the Ripper": "", "Johnny": "",
+                "KeepNote": "", "KillerBee": "", "Kismet": "",
+                "Linux Exploit Suggester": "", "Lynis": "", "MagicTree": "",
+                "Maltego Teeth": "", "Maskprocessor": "", "Metagoofil": "",
+                "Miranda": "", "Multimon-NG": "", "Ncrack": "",
+                "Nipper-ng": "", "Nishang": "", "Nmap": "", "OllyDbg": "",
+                "Oscanner": "", "PACK": "", "Parsero": "", "PixieWPS": "",
+                "PowerSploit": "", "Powerfuzzer": "", "RSMangler": "",
+                "RTLSDR Scanner": "", "RainbowCrack": "", "Reaver": "",
+                "Recon-ng": "", "RegRipper": "", "RidEnum": "", "SET": "",
+                "SIPArmyKnife": "", "SIPVicious": "", "SIPp": "",
+                "SQLdict": "", "SSLsplit": "", "SSLyze": "", "Sakis3G": "",
+                "ShellNoob": "", "SidGuesser": "", "SlowHTTPTest": "",
+                "SniffJoke": "", "Spooftooph": "", "Sqlninja": "",
+                "Statsprocessor": "", "THC-Hydra": "", "THC-IPV6": "",
+                "THC-SSL-DOS": "", "THC-pptp-bruter": "", "TLSSLed": "",
+                "Termineter": "", "TrueCrack": "", "U3-Pwn": "",
+                "URLCrazy": "", "Valgrind": "", "VoIPHopper": "",
+                "Volatility": "", "WOL-E": "", "WebScarab": "",
+                "Webshells": "", "Weevely": "", "Wifi Honey": "",
+                "Wifitap": "", "Wifite": "", "Winexe": "", "Wireshark": "",
+                "Xplico": "", "YARA": "", "Yersinia": "", "acccheck": "",
+                "ace-voip": "", "android-sdk": "", "apktool": "",
+                "bing-ip2hosts": "", "braa": "", "bulk-extractor": "",
+                "chntpw": "", "cisco-auditing-tool": "",
+                "cisco-global-exploiter": "", "cisco-ocs": "",
+                "cisco-torch": "", "coWPAtty": "", "copy-router-config": "",
+                "crackle": "", "creddump": "", "crunch": "", "dbd": "",
+                "dc3dd": "", "ddrescue": "", "dex2jar": "", "diStorm3": "",
+                "dnmap": "", "dns2tcp": "", "dnsenum": "", "dnsmap": "",
+                "dnstracer": "", "dnswalk": "", "dos2unix": "",
+                "eapmd5pass": "", "edb-debugger": "", "enum4linux": "",
+                "enumIAX": "", "exploitdb": "", "extundelete": "", "fiked": "",
+                "findmyhash": "", "fragroute": "", "fragrouter": "",
+                "goofile": "", "gpp-decrypt": "", "gr-scan": "",
+                "hamster-sidejack": "", "hash-identifier": "", "hping3": "",
+                "http-tunnel": "", "iPhone Backup Analyzer": "", "iSMTP": "",
+                "iaxflood": "", "inviteflood": "", "ipv6-toolkit": "",
+                "isr-evilgrade": "", "jSQL": "", "jad": "", "javasnoop": "",
+                "jboss-autopwn": "", "kalibrate-rtl": "", "keimpx": "",
+                "lbd": "", "masscan": "", "mdk3": "", "mfcuk": "", "mfoc": "",
+                "mfterm": "", "mitmproxy": "", "multiforcer": "", "ntop": "",
+                "oclgausscrack": "", "ohrwurm": "",
+                "openvas-administrator": "", "openvas-cli": "",
+                "openvas-manager": "", "openvas-scanner": "", "p0f": "",
+                "patator": "", "pdf-parser": "", "pdfid": "", "pdgmail": "",
+                "peepdf": "", "phrasendrescher": "", "pipal": "",
+                "polenum": "", "protos-sip": "", "pwnat": "", "rcracki-mt": "",
+                "rebind": "", "redfang": "", "responder": "", "rtpbreak": "",
+                "rtpflood": "", "rtpinsertsound": "", "rtpmixsound": "",
+                "sbd": "", "sctpscan": "", "sfuzz": "", "smali": "",
+                "smtp-user-enum": "", "snmpcheck": "", "sqlmap": "",
+                "sqlsus": "", "sslcaudit": "", "sslstrip": "", "t50": "",
+                "theHarvester": "", "tnscmd10g": "", "twofi": "",
+                "unix-privesc-check": "", "wordlists": "", "xspy": "",
+                "zaproxy": ""}
 
-info = {"": ""}
+info = {"1": "acccheck", "2": "ace-voip", "3": "Amap", "4": "Automater",
+        "5": "bing-ip2hosts", "6": "braa", "7": "CaseFile", "8": "CDPSnarf",
+        "9": "cisco-torch", "10": "Cookie Cadger", "11": "copy-router-config",
+        "12": "DMitry", "13": "dnmap", "14": "dnsenum", "15": "dnsmap",
+        "16": "DNSRecon", "17": "dnstracer", "18": "dnswalk",
+        "19": "DotDotPwn", "20": "enum4linux", "21": "enumIAX",
+        "22": "exploitdb", "23": "Fierce", "24": "Firewalk", "25": "fragroute",
+        "26": "fragrouter", "27": "Ghost Phisher", "28": "GoLismero",
+        "29": "goofile", "30": "hping3", "31": "InTrace", "32": "iSMTP",
+        "33": "lbd", "34": "Maltego Teeth", "35": "masscan",
+        "36": "Metagoofil", "37": "Miranda", "38": "Nmap", "39": "ntop",
+        "40": "p0f", "41": "Parsero", "42": "Recon-ng", "43": "SET",
+        "44": "smtp-user-enum", "45": "snmpcheck", "46": "sslcaudit",
+        "47": "SSLsplit", "48": "sslstrip", "49": "SSLyze", "50": "THC-IPV6",
+        "51": "theHarvester", "52": "TLSSLed", "53": "twofi", "54": "URLCrazy",
+        "55": "Wireshark", "56": "WOL-E", "57": "Xplico"}
 
-vulnerability = {"": ""}
 
-wifi = {"": ""}
+vulnerability = {"1": "BBQSQL", "2": "BED", "3": "cisco-auditing-tool",
+                 "4": "cisco-global-exploiter", "5": "cisco-ocs",
+                 "6": "cisco-torch", "7": "copy-router-config",
+                 "8": "DBPwAudit", "9": "Doona", "10": "DotDotPwn",
+                 "11": "Greenbone Security Assistant", "12": "GSD",
+                 "13": "HexorBase", "14": "Inguma", "15": "jSQL",
+                 "16": "Lynis", "17": "Nmap", "18": "ohrwurm",
+                 "19": "openvas-administrator", "20": "openvas-cli",
+                 "21": "openvas-manager", "22": "openvas-scanner",
+                 "23": "Oscanner", "24": "Powerfuzzer", "25": "sfuzz",
+                 "26": "SidGuesser", "27": "SIPArmyKnife",
+                 "28": "sqlmap", "29": "Sqlninja", "30": "sqlsus",
+                 "31": "THC-IPV6", "32": "tnscmd10g",
+                 "33": "unix-privesc-check", "34": "Yersinia"}
 
-sniff_n_spoof = {"": ""}
+wifi = {"1": "Aircrack-ng", "2": "Asleap", "3": "Bluelog", "4": "BlueMaho",
+        "5": "Bluepot", "6": "BlueRanger", "7": "Bluesnarfer", "8": "Bully",
+        "9": "coWPAtty", "10": "crackle", "11": "eapmd5pass",
+        "12": "Fern Wifi Cracker", "13": "Ghost Phisher", "14": "GISKismet",
+        "15": "Gqrx", "16": "gr-scan", "17": "kalibrate-rtl",
+        "18": "KillerBee", "19": "Kismet", "20": "mdk3", "21": "mfcuk",
+        "22": "mfoc", "23": "mfterm", "24": "Multimon-NG", "25": "PixieWPS",
+        "26": "Reaver", "27": "redfang", "28": "RTLSDR Scanner",
+        "29": "Spooftooph", "30": "Wifi Honey", "31": "Wifitap",
+        "32": "Wifite"}
 
-access = {"": ""}
+sniff_n_spoof = {"1": "Burp Suite", "2": "DNSChef", "3": "fiked",
+                 "4": "hamster-sidejack", "5": "HexInject", "6": "iaxflood",
+                 "7": "inviteflood", "8": "iSMTP", "9": "isr-evilgrade",
+                 "10": "mitmproxy", "11": "ohrwurm", "12": "protos-sip",
+                 "13": "rebind", "14": "responder", "15": "rtpbreak",
+                 "16": "rtpinsertsound", "17": "rtpmixsound", "18": "sctpscan",
+                 "19": "SIPArmyKnife", "20": "SIPp", "21": "SIPVicious",
+                 "22": "SniffJoke", "23": "SSLsplit", "24": "sslstrip",
+                 "25": "THC-IPV6", "26": "VoIPHopper", "27": "WebScarab",
+                 "28": "Wifi Honey", "29": "Wireshark", "30": "xspy",
+                 "31": "Yersinia", "32": "zaproxy"}
 
-report = {"": ""}
+access = {"1": "CryptCat", "2": "Cymothoa", "3": "dbd", "4": "dns2tcp",
+          "5": "http-tunnel", "6": "HTTPTunnel", "7": "Intersect",
+          "8": "Nishang", "9": "polenum", "10": "PowerSploit", "11": "pwnat",
+          "12": "RidEnum", "13": "sbd", "14": "U3-Pwn", "15": "Webshells",
+          "16": "Weevely", "17": "Winexe"}
 
-exploit = {"": ""}
+report = {"1": "CaseFile", "2": "CutyCapt", "3": "dos2unix", "4": "Dradis",
+          "5": "KeepNote", "6": "MagicTree", "7": "Metagoofil",
+          "8": "Nipper-ng", "9": "pipal"}
 
-forensics = {"": ""}
+exploit = {"1": "Armitage", "2": "Backdoor Factory", "3": "BeEF",
+           "4": "cisco-auditing-tool", "5": "cisco-global-exploiter",
+           "6": "cisco-ocs", "7": "cisco-torch", "8": "Commix", "9": "crackle",
+           "10": "jboss-autopwn", "11": "Linux Exploit Suggester",
+           "12": "Maltego Teeth", "13": "SET", "14": "ShellNoob",
+           "15": "sqlmap", "16": "THC-IPV6", "17": "Yersinia"}
 
-stress_test = {"": ""}
+forensics = {"1": "Binwalk", "2": "bulk-extractor", "3": "Capstone",
+             "4": "chntpw", "5": "Cuckoo", "6": "dc3dd", "7": "ddrescue",
+             "8": "DFF", "9": "diStorm3", "10": "Dumpzilla",
+             "11": "extundelete", "12": "Foremost", "13": "Galleta",
+             "14": "Guymager", "15": "iPhone Backup Analyzer", "16": "p0f",
+             "17": "pdf-parser", "18": "pdfid", "19": "pdgmail",
+             "20": "peepdf", "21": "RegRipper", "22": "Volatility",
+             "23": "Xplico"}
 
-password = {"": ""}
+stress_test = {"1": "DHCPig", "2": "FunkLoad", "3": "iaxflood",
+               "4": "Inundator", "5": "inviteflood", "6": "ipv6-toolkit",
+               "7": "mdk3", "8": "Reaver", "9": "rtpflood",
+               "10": "SlowHTTPTest", "11": "t50", "12": "Termineter",
+               "13": "THC-IPV6", "14": "THC-SSL-DOS"}
 
-rev_engineer = {"": ""}
+password = {"1": "acccheck", "2": "Burp Suite", "3": "CeWL", "4": "chntpw",
+            "5": "cisco-auditing-tool", "6": "CmosPwd", "7": "creddump",
+            "8": "crunch", "9": "DBPwAudit", "10": "findmyhash",
+            "11": "gpp-decrypt", "12": "hash-identifier", "13": "HexorBase",
+            "14": "THC-Hydra", "15": "John the Ripper", "16": "Johnny",
+            "17": "keimpx", "18": "Maltego Teeth", "19": "Maskprocessor",
+            "20": "multiforcer", "21": "Ncrack", "22": "oclgausscrack",
+            "23": "PACK", "24": "patator", "25": "phrasendrescher",
+            "26": "polenum", "27": "RainbowCrack", "28": "rcracki-mt",
+            "29": "RSMangler", "30": "SQLdict", "31": "Statsprocessor",
+            "32": "THC-pptp-bruter", "33": "TrueCrack", "34": "WebScarab",
+            "35": "wordlists", "36": "zaproxy"}
 
-hw_hacking = {"": ""}
+rev_engineer = {"1": "apktool", "2": "dex2jar", "3": "diStorm3",
+                "4": "edb-debugger", "5": "jad", "6": "javasnoop",
+                "7": "JD-GUI", "8": "OllyDbg", "9": "smali",
+                "10": "Valgrind", "11": "YARA"}
 
-extras = {"": ""}
+hw_hacking = {"1": "android-sdk", "2": "apktool", "3": "Arduino",
+              "4": "dex2jar", "5": "Sakis3G", "6": "smali"}
+
 
 categories = [info, vulnerability, wifi, sniff_n_spoof, access, report,
               exploit, forensics, stress_test, password, rev_engineer,
-              hw_hacking, extras]
+              hw_hacking]
