74d92cfff583ef3a03c1b2bb1f9f8e9735b31046
sslavov93 <sslavov93@gmail.com>
2016-09-02 12:04:01 +0300

    Updated Setup and Cleanup scripts

--- scripts/util/setup.sh
+++ scripts/util/setup.sh
@@ -1,4 +1,4 @@
 sudo apt-key adv --keyserver pgp.mit.edu --recv-keys ED444FF07D8D0BF6
 sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y update
-sudo apt-get -f install
+sudo apt-get -f -y install
 
--- scripts/util/teardown.sh
+++ scripts/util/teardown.sh
@@ -1,1 +1,1 @@
-sudo apt-get autoremove
+sudo apt-get -y autoremove
